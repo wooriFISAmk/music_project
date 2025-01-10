ab2c65beb74d0b8478ec32cf710f2e060140be7f
nanahj <dlguswjd221@naver.com>
2025-01-10 15:30:23 +0900

    진.짜 노래추가

--- music_project/src/controller/SongController.java
+++ music_project/src/controller/SongController.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.sql.SQLException;
 
+import model.SongDAO;
+import model.dto.SongDTO;
 import service.SongServiceImp;
 import view.EndView;
 
@@ -9,6 +11,17 @@ public class SongController {
 
 	static SongServiceImp s = SongServiceImp.getInstance();
 	
+	// 새로운 노래 추가 로직
+	public static boolean createSong(SongDTO song) {
+		boolean result = false;
+		try {
+			result = SongDAO.createSong(song);
+		} catch (SQLException s) {
+			s.printStackTrace();
+		}
+		return result;
+	}
+		
 	public static void getAllSongs() {
 		try {
 			EndView.songListView(s.getAllSongs());
