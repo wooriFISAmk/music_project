4ed11e4586a9d8631e510dee5b8517ca97089501
BlueRedOrange <jmg0916789@gmail.com>
2025-01-10 14:33:14 +0900

    중간 수정

--- music_project/src/controller/SongController.java
+++ music_project/src/controller/SongController.java
@@ -1,5 +1,19 @@
 package controller;
 
+import java.sql.SQLException;
+
+import service.SongService;
+
 public class SongController {
+
+	public static void updateUrlBySongName(String songName, String url) {
+		// TODO Auto-generated method stub
+		try {
+			SongService.updateUrlBySongName(songName, url);
+		} catch(SQLException e){
+			//e.printStackTrace();
+			System.out.println("error");
+		}
+	}
 
 }
