0793e91560d08beca785d1512cbbe27679578504
BlueRedOrange <jmg0916789@gmail.com>
2025-01-10 10:55:24 +0900

    쿼리문 수정

--- db/song.DDL.sql
+++ db/song.DDL.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS song;
 -- table 생성
 CREATE TABLE song (
        song_id INT AUTO_INCREMENT PRIMARY KEY,
-       artist VARCHAR(10) NOT NULL,
-       song   VARCHAR(30) NOT NULL,
+       artist VARCHAR(30) NOT NULL,
+       song VARCHAR(30) NOT NULL,
        genre ENUM('랩','가요','팝','락','인디','댄스','힙합','드라마','R&B', 'OST', '발라드'),
        artType TINYINT,
        url VARCHAR(100)
